f210dde50739694d9d20ab8a79115b7e0450c39d
LizaFedorova <lizafdrv49@gmail.com>
2022-11-19 14:52:01 +0500

    Правда или действие (часть 2)

--- js/big-picture.js
+++ js/big-picture.js
@@ -1,46 +1,79 @@
 import { getPhotosDescription } from './mocks.js';
-import { getThumbnails } from './thumbnails.js';
+import { getThumbnails, getCommentTemplate } from './thumbnails.js';
+import { MAX_SHOWN_COMMENTS } from './consts.js';
 
 const bigPicture = document.querySelector('.big-picture');
 const closeButton = document.querySelector('.big-picture__cancel');
+const commentsList = bigPicture.querySelector('.social__comments');
+const shownCommentsCount = document.querySelector('.social__comment-count');
+const commentsLoader = bigPicture.querySelector('.comments-loader');
+let actualComments = [];
+let renderedCommentsCount  = MAX_SHOWN_COMMENTS;
 
-const renderComments = (comments) => {
-  const fragment = document.createDocumentFragment();
-  const socialComments = document.querySelector('.social__comments');
+const getCounterCommentsTemplate = (commentsCount) => `${Math.min(renderedCommentsCount, commentsCount)} из <span class="comments-count">${commentsCount}</span> комментариев`;
 
-  comments.forEach((comment) => {
-    const newComment = document.querySelector('.social__comment').cloneNode(true);
-    newComment.querySelector('.social__picture').src = comment.avatar;
-    newComment.querySelector('.social__picture').alt = comment.name;
-    newComment.querySelector('.social__text').textContent = comment.message;
-    fragment.appendChild(newComment);
-  });
+const getCounterComments = () => {
+  shownCommentsCount.innerHTML = '';
+  shownCommentsCount.insertAdjacentHTML('afterbegin', getCounterCommentsTemplate(actualComments.length));
+};
 
-  socialComments.innerHTML = '';
-  socialComments.appendChild(fragment);
+const renderComments = () => {
+  getCounterComments();
+  commentsList.innerHTML = '';
+  const commentsTemplate = actualComments.slice(0, renderedCommentsCount).map((comment) => getCommentTemplate(comment)).join('');
+  commentsList.insertAdjacentHTML('afterbegin', commentsTemplate);
+
+  if (renderedCommentsCount >= actualComments.length) {
+    commentsLoader.removeEventListener('click', onCommentsButtonLoader);
+    commentsLoader.classList.add('hidden');
+  }
+};
+
+const initComments = (comments) => {
+  actualComments = comments.slice();
+  commentsList.innerHTML='';
+
+  if (comments.length === 0) {
+    commentsLoader.classList.add('hidden');
+    shownCommentsCount.innerHTML = 'Нет комментариев';
+    return;
+  }
+
+  renderComments();
+  commentsLoader.addEventListener('click', onCommentsButtonLoader);
 };
 
 const renderBigPicture = (picture) => {
   bigPicture.querySelector('.big-picture__img').querySelector('img').src = picture.url;
   bigPicture.querySelector('.likes-count').textContent = picture.likes;
   bigPicture.querySelector('.social__caption').textContent = picture.description;
-  bigPicture.querySelector('.comments-count').textContent = picture.comments.length;
-  bigPicture.querySelector('.social__comment-count').classList.add('hidden');
-  bigPicture.querySelector('.comments-loader').classList.add('hidden');
-  renderComments(picture.comments);
+  initComments(picture.comments);
 };
 
 const closePicture = () => {
   document.body.classList.remove('modal-open');
   bigPicture.classList.add('hidden');
+  closeButton.removeEventListener('click', onCloseButton);
+  document.removeEventListener('keydown', onEscKeyDown);
+  commentsLoader.classList.remove('hidden');
+  commentsLoader.removeEventListener('click', onCommentsButtonLoader);
+  renderedCommentsCount = MAX_SHOWN_COMMENTS;
 };
 
-const onEscKeyDown = (evt) => {
+function onEscKeyDown(evt) {
   if (evt.key === 'Escape') {
     closePicture();
-    document.removeEventListener('keydown', onEscKeyDown);
   }
-};
+}
+
+function onCloseButton() {
+  closePicture();
+}
+
+function onCommentsButtonLoader() {
+  renderedCommentsCount += MAX_SHOWN_COMMENTS;
+  renderComments();
+}
 
 const openPicture = (element) => {
   document.body.classList.add('modal-open');
